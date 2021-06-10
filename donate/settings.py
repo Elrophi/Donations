747c675970be106d7d0ec4331863bb44ebec6144
el <elrophilskwaila@gmail.com>
2021-06-11 00:37:53 +0300

    heroku files

--- donate/settings.py
+++ donate/settings.py
@@ -15,6 +15,9 @@ import os
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
